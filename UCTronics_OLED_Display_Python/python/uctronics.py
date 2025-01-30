440f4afaed41637784a7fd4b46595367dd2da502
Matthew <animeoverseer@gmail.com>
2025-01-29 21:39:43 -0600

    sync

--- UCTronics_OLED_Display_Python/python/uctronics.py
+++ UCTronics_OLED_Display_Python/python/uctronics.py
@@ -224,7 +224,7 @@ class UCB86(object):
         pass
 
     def sync(self):
-        self.__i2c_write_command(SYNC_REG, 0x00, 0x00);
+        self.__i2c_write_command(SYNC_REG, 0x00, 0x01);
 
     def image(self, x=0, y=0, w=ST7735_WIDTH, h=ST7735_HEIGHT, data=[]):
         col = h - y
